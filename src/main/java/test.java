c285b229553fe2e140f137d725ee1f5fb503ec6f
Shiladitya <shiladitya92bose@gmail.com>
2023-07-29 00:30:54 +0530

    first commit

---
+++ src/main/java/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		int i[] = new int[5];
+		for(int j=0;j<5;j++){
+			i[j] = j;
+		}
+
+		System.out.println(i);
+		//for (int a : 7 );
+	}
+
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void checkGoogle() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
