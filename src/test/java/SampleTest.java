41dff1cdc4a541675f2635c39ce24de70745d1da
Shiladitya <shiladitya92bose@gmail.com>
2023-09-26 07:52:01 +0530

    Updated SampleTest

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,25 +1,52 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SampleTest {
 
-    @Test
-    public void checkGoogle() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://ecommerce-playground.lambdatest.io/index.php");
-        Thread.sleep(3000);
-        driver.quit();
-    }
+    @Test(groups = "web")
+    public void checkSelenoid() throws MalformedURLException, InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserVersion", "112.0");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "Test badge...");
 
-    @Test
-    public void checkFacebook() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.facebook.com");
-        driver.manage().window().maximize();
-        System.out.println("Test");
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to add "trash" button */
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            /* How to enable video recording */
+            put("enableVNC",true);
+            put("enableVideo", true);
+
+            put("videoName","sample.mp4");
+        }});
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("Testing mini Bytes", Keys.ENTER);
+        Thread.sleep(3000);
         driver.quit();
     }
 }
